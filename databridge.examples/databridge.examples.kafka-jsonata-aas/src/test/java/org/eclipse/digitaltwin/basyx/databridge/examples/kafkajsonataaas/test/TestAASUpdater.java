e59ae827dd25868f64a68d3963332f7683be8eff
Jannis Jung <jannis.jung@iese.fraunhofer.de>
2024-07-19 00:14:27 +0200

    fixes kafka-jsonata-test
    Signed-off-by: Jannis Jung <jannis.jung@iese.fraunhofer.de>

--- databridge.examples/databridge.examples.kafka-jsonata-aas/src/test/java/org/eclipse/digitaltwin/basyx/databridge/examples/kafkajsonataaas/test/TestAASUpdater.java
+++ databridge.examples/databridge.examples.kafka-jsonata-aas/src/test/java/org/eclipse/digitaltwin/basyx/databridge/examples/kafkajsonataaas/test/TestAASUpdater.java
@@ -137,7 +137,7 @@ public class TestAASUpdater {
 		ConnectedAssetAdministrationShell aas = getAAS(deviceAASId);
 
 		ISubmodelElement updatedProp = getSubmodelElement(aas, "ConnectedSubmodel", propertyIdShort);
-		
+		System.out.println(updatedProp.getValue());
 		return updatedProp.getValue();
 	}
 
